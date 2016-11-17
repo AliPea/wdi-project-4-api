343e522a22b3e66ce73e809f1411727a1a811798
AliPea <pearseindeas@gmail.com>
2016-11-17 17:09:15 +0000

    added image to User schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109205512) do
+ActiveRecord::Schema.define(version: 20161117170603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161109205512) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
+    t.string "image"
   end
 
   add_foreign_key "drinks", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,12 +35,12 @@ d2 = u2.drinks.create!(name: "Blue Moon", quantity: 4, units: 2, price: 3.00 )
 d3 = u1.drinks.create!(name: "Meantime", quantity: 6, units: 2, price: 3.00 )
 d4 = u3.drinks.create!(name: "Anchor Steam", quantity: 2, units: 2, price: 3.00 )
 
-[
-  { name: "Beer", image: "http://www.menshealth.com/sites/menshealth.com/files/styles/slideshow-desktop/public/images/slideshow2/beer-intro.jpg?itok=hhBQBwWj" },
-  { name: "Wine", image: "http://images.r.cruisecritic.co.uk/features/ships/top-25-tips/best-wine-hero.jpg" },
-  { name: "Gin and Tonic", image: "http://goodthingsmagazine.com/wp-content/uploads/2015/05/Gin-and-tonic.jpg" },
-  { name: "Cider", image: "http://www.nycider.com/images/hard-cider-lg.jpg" },
-  { name: "Rum and Coke", image: "http://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2015/02/10223_cuba_libre_3000.jpg" },
-].each do |menu|
-  Menu.create!(menu)
-end
+# [
+#   { name: "Beer", image: "http://www.menshealth.com/sites/menshealth.com/files/styles/slideshow-desktop/public/images/slideshow2/beer-intro.jpg?itok=hhBQBwWj" },
+#   { name: "Wine", image: "http://images.r.cruisecritic.co.uk/features/ships/top-25-tips/best-wine-hero.jpg" },
+#   { name: "Gin and Tonic", image: "http://goodthingsmagazine.com/wp-content/uploads/2015/05/Gin-and-tonic.jpg" },
+#   { name: "Cider", image: "http://www.nycider.com/images/hard-cider-lg.jpg" },
+#   { name: "Rum and Coke", image: "http://search.chow.com/thumbnail/1280/800/www.chowstatic.com/assets/2015/02/10223_cuba_libre_3000.jpg" },
+# ].each do |menu|
+#   Menu.create!(menu)
+# end
